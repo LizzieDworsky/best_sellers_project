fb2967fbc3763927f387ea6e39020014a167a08a
LizzieDworsky <amydworsky15@gmail.com>
2022-09-14 13:17:49 -0600

    added logic to instantiate all books as objects and add to a list

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from book import Book
 from data import data_list
 
 def create_book_list(book_class, data_set={}):
-    pass
+    temp_list = []
+    for book in data_set:
+        temp_list.append(book_class(book))
+    return temp_list
 
 def custom_filter(lambda_function, book_list):
     pass
