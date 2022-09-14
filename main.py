a5890e0d2a3ba02837de0be01d8ef3e56966fc1e
LizzieDworsky <amydworsky15@gmail.com>
2022-09-14 14:13:41 -0600

    refactored first task + added additional task instructions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from book import Book
 from data import data_list
 
+# Should be using both lambda and list comprehension for each evaluation
+
 def create_book_list(book_class, data_set={}):
     temp_list = []
     for book in data_set:
@@ -17,16 +19,44 @@ def custom_filter(lambda_function, book_list):
 
 book_list = create_book_list(Book, data_list)
 
-books_2018 = [book for book in book_list if book.year == 2018]
-
-# least_reviews_2018 = custom_filter_get(lambda x : x.number_of_reviews, books_2018)
-books_2018_2 = custom_filter(lambda item : item.year == 2018, book_list)
+# books_2018_2 = custom_filter(lambda item : item.year == 2018, book_list)
 
 # for book in books_2018_2:
 #     print(book.year)
 
-current_book = books_2018[0]
-for book in books_2018:
-    if current_book.number_of_reviews > book.number_of_reviews:
-        current_book = book
-print(current_book.name)
+# (5 points): As a data analyst, I want to determine what book had the lowest number of reviews in 2018. (Analysis 1)
+
+def least_reviews_2018(list_of_books):
+    books_2018 = [book for book in list_of_books if book.year == 2018]
+    current_book = books_2018[0]
+    for book in books_2018:
+        if current_book.number_of_reviews > book.number_of_reviews:
+            current_book = book
+    print(current_book.name)
+
+# least_reviews_2018(book_list)
+
+
+
+# (5 points): As a data analyst, I want to determine which genre (fiction/non-fiction) has appeared the most in the top 50s list and print the result to the terminal. (Analysis 2)
+
+
+
+
+
+
+# (5 points): As a data analyst, I want to determine what book has been in the top 50s list for the most years and print the book’s title and the number of times 
+# it has appeared in the list to the terminal. (Analysis 3)
+
+# (10 points): As a data analyst, I want to utilize lambda functions and list comprehension within the code responsible for executing each evaluation question solution.
+
+
+# Bonus
+
+# (5 points): As a data analyst, I want to determine what author has shown up on the top 50’s list the most and display the author in the terminal. (Bonus Analysis 1)
+# HINT: Only count distinct books by that author - Do not include the same book in different years within the calculation
+
+# (5 points): As a data analyst, I want to display the top book for each year based on the book’s user rating & the number of reviews. (Bonus Analysis 2)
+# HINT: Find the top book by ordering by user rating and then ordering by number of reviews. Print the year and the book’s title to the terminal. 
+
+# (5 points): As a data analyst, I want to determine which book has appeared the most in the top 50s list consecutively. (Bonus Analysis 2)
