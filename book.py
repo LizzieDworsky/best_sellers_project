c144f1b492664e251c84e4247ba86e876e1117e0
LizzieDworsky <amydworsky15@gmail.com>
2022-09-14 12:34:51 -0600

    fixed Book class values, added bones for OOP and filtering data

--- book.py
+++ book.py
@@ -2,10 +2,10 @@
 
 class Book:
     def __init__(self, book={}):
-        self.name = book.name
-        self.author = book.author
-        self.user_rating = book.user_rating
-        self.number_of_reviews = book.number_of_reviews
-        self.price = book.price
-        self.year = book.year
-        self.genre = book.genre
+        self.name = book["name"]
+        self.author = book["author"]
+        self.user_rating = book["user_rating"]
+        self.number_of_reviews = book["number_of_reviews"]
+        self.price = book["price"]
+        self.year = book["year"]
+        self.genre = book["genre"]
--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 from book import Book
+from data import data_list
 
+def create_book_list(book_class, data_set={}):
+    pass
+
+def custom_filter(lambda_function, book_list):
+    pass
+
+book_list = create_book_list(Book, data_list)
