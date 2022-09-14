7992d58b6f22fac7d197a0f1f3e63bb07e978eb0
LizzieDworsky <amydworsky15@gmail.com>
2022-09-14 12:23:17 -0600

    create book class, added main file

---
+++ book.py
@@ -0,0 +1,11 @@
+
+
+class Book:
+    def __init__(self, book={}):
+        self.name = book.name
+        self.author = book.author
+        self.user_rating = book.user_rating
+        self.number_of_reviews = book.number_of_reviews
+        self.price = book.price
+        self.year = book.year
+        self.genre = book.genre
